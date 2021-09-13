3234f018f7ab1b035faabbb3d63fa8ac9ad80b95
李世馨 <lishi-xin@lishixindeMacBook-Pro.local>
2021-09-13 15:16:41 +0800

    change code style

--- getData/crawling.py
+++ getData/crawling.py
@@ -4,11 +4,12 @@ import pandas
 
 def hadleDate(soup):
     cleanDate = []
-    filterDate = soup.find_all('td',attrs={'style':'font-size: 6vmin; font-weight: bold; color: #000000;border-bottom-style: dotted; border-bottom'})
+    filterDate = soup.find_all('td',attrs={'style':'font-size: 4vmin; font-weight: bold; color: #000000;border-bottom-style: dotted; border-bottom-color: #CCCCCC;text-align:center;padding:0px;vertical-align:middle'})
     for i in filterDate:
         x = i.text
         x = x[:10]
         cleanDate.append(x)
+    cleanDate.reverse()
     return cleanDate
 
 def hadleLottoNumber(soup):
@@ -17,6 +18,7 @@ def hadleLottoNumber(soup):
     for i in filterNumber:
         x = "".join((i.text.strip()).split()) 
         cleanLottoNumber.append(x)
+    cleanLottoNumber.append(x)
     return cleanLottoNumber
 
 def getPageData(webURL):
@@ -31,3 +33,16 @@ def getPageData(webURL):
         lottoNumber += hadleLottoNumber(soup)
 
     return date,lottoNumber
+
+def getMorePages(num):
+    allPageDate = []
+    allPageLottoNumber = []
+
+    for i in range (num):
+        pageURL = f'https://www.pilio.idv.tw/lto539/list.asp?indexpage={191-i}&orderby=new'
+        allPageDate += getPageData(webURL=pageURL)[0]
+        allPageLottoNumber += getPageData(webURL=pageURL)[1]
+
+    return allPageDate, allPageLottoNumber
+
+getMorePages(5)
